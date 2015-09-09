155dda29cd1fbcb824bd2fe9b400ffdceb6d0e2f
Miguel Garcia Puyol <miguel.garcia.puyol@sap.com>
2015-09-09 21:17:57 +0200

    Reworking the workflow to list more options. Check the readme for the changes

--- README.md
+++ README.md
@@ -20,6 +20,7 @@ Type `jira` on Alfred to list the
 
 | Command | Description |
 | ------- | ------------|
+| `jira`                 | Show the Jira options |
 | `setupjiraserver`      | Set the jira server            |
 | `setupjirauser`        | Set the jira username          |
 | `setupjirakeychain`    | Set the Apple Keychain service where your JIRA password is stored |
@@ -28,11 +29,20 @@ Type `jira` on Alfred to list the
 
 # TODO
 
+## v1.1
+- [x] List the sprints
+- [x] Show menu with story data
+- [x] List subtasks of a given story
+- [x] Show the assigned person
+
+## Roadman
+- [ ] List comments of a given story / subtask
 - [ ] Provide screenshots of the usage
 - [ ] Filter the listed stories
 - [ ] Select the Board from Alfred and not with an ID
 - [ ] Refactor the setup method to avoid code duplication
-- [ ] List subtasks of a given story
 - [ ] Add a comment to a story/subtask
 - [ ] Create a story
 - [ ] Create a subtask 
+- [ ] Search by name
+- [ ] Search by Key
--- jira.py
+++ jira.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # encoding: utf-8
-
+import argparse
 
 import sys
 
@@ -10,9 +10,10 @@ from jira import JIRA
 log = None
 jira = None
 board_id = None
+wf = None
 
 
-def list_sprints(wf):
+def list_sprints():
     sprints = wf.cached_data('sprints', get_sprints, max_age=360)
 
     if sprints:
@@ -24,8 +25,8 @@ def list_sprints(wf):
         wf.add_item(title='No sprints found', subtitle='try later')
 
 
-def list_incomplete_stories(wf):
-    # incompleted_stories = wf.cached_data('incompleted_stories', get_incompleted_stories, max_age=60)
+def list_incomplete_stories():
+    # incompleted_stories = wf.cached_data('stories', get_incompleted_stories, max_age=60)
     #
     incompleted_stories = get_incompleted_stories()
     for story in incompleted_stories:
@@ -33,7 +34,8 @@ def list_incomplete_stories(wf):
             log.info(story.key)
             title = ' - '.join([story.key, story.summary])
             subtitle = ' - '.join([story.priorityName, story.status.name])
-            wf.add_item(title=title, subtitle=subtitle, arg=story.permalink(), valid=True)
+            wf.add_item(title=title, subtitle=subtitle, arg=story.permalink(), autocomplete='--story ' + story.key)
+            wf.store_data('key', story.key)
 
 
 
@@ -56,7 +58,7 @@ def get_incompleted_stories():
 
 
 
-def setup(wf):
+def setup():
     username = wf.settings.get('user', None)
     keychain = wf.settings.get('keychain', None)
     server = wf.settings.get('server', None)
@@ -74,18 +76,106 @@ def setup(wf):
             wf.send_feedback()
     return None
 
+
+def list_subtasks(story_key):
+    issue = get_issue(story_key)
+    subtasks = issue.fields.subtasks
+    # wf.add_item(title='Create a new subtask', autocomplete='--add_subtask ' + story_key)
+    if subtasks:
+        for subtask in subtasks:
+            title = ' - '.join([subtask.key, subtask.fields.summary])
+            subtitle = ' - '.join([subtask.fields.priority.name, subtask.fields.status.name])
+            wf.add_item(title=title, subtitle=subtitle, arg=subtask.permalink(), autocomplete='--subtask ' + subtask.key)
+    else:
+        wf.add_item(title='The story has no subtasks', subtitle='Click to create one (not yet)', valid=False)
+    wf.add_item(title='Go back', autocomplete='--story ' + story_key)
+
+def _load_issue():
+    story_key = wf.cached_data('story-key')
+    return jira.issue(story_key)
+
+
+def get_issue(story_key):
+    wf.cache_data('story-key', story_key)
+    # story = wf.cached_data('story', _load_issue, max_age=30)
+    # return story
+    return _load_issue()
+
+def show_story_options(story_key, is_subtask=False):
+    issue = get_issue(story_key)
+    wf.add_item(title='Open in Browser', subtitle='opens in your default browser', arg=issue.permalink(), valid=True)
+    if not is_subtask:
+        wf.add_item(title='Browse Subtasks', subtitle='Show all the tasks of the story', autocomplete='--subtasks ' + story_key + ' ')
+    wf.add_item(title='Status: ' + issue.fields.status.name, subtitle=issue.fields.status.description)
+    if issue.fields.assignee:
+        wf.add_item(title='Assignee: ' + issue.fields.assignee.displayName, subtitle=issue.fields.assignee.name)
+    wf.add_item(title='Reporter: ' + issue.fields.creator.displayName, subtitle=issue.fields.creator.name)
+    # TODO: Show comments option
+    if not is_subtask:
+        wf.add_item(title='Go back', autocomplete='')
+    else:
+        wf.add_item(title='Go back', autocomplete='--subtasks ' + issue.parent.key)
+
+
+def show_subtask_options(story_key, title):
+    wf.add_item(title='Creating Subtask For: ' + story_key, subtitle=title)
+    wf.store_data('subtask_title', title)
+    wf.add_item(title='Save it!', subtitle=title, autocomplete='--save')
+
+
+def create_subtask(parent_key, description):
+    log.info(' '.join('Creating subtask', description, 'for story', parent_key))
+    parent_story = get_issue(parent_key)
+    subtask_fields = {
+        'project' : { 'key': parent_story.fields.project.key },
+        'summary' : 'Test child auto created issue',
+        'description' : description,
+        'issuetype' : { 'name' : 'Sub-task' },
+        'parent' : { 'id' : parent_story.key},
+    }
+
+    jira.create_issue(fields=subtask_fields)
+
 def main(wf):
     log.info(wf.args)
     # wf.add_item(title='Loading Sprints', subtitle='test')
     # wf.send_feedback()
     # list_sprints(wf)
-    list_incomplete_stories(wf)
-    wf.send_feedback()
+    # log.info(wf.args)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--story', dest='story', nargs='?', default=None)
+    parser.add_argument('--subtasks', dest='subtasks', nargs='?', default=None)
+    parser.add_argument('--subtask', dest='subtask', nargs='?', default=None)
+    parser.add_argument('--add_subtasks', dest='add_subtask', nargs='?', default=None)
+    # parser.add_argument('--save', dest='save_subtask', nargs='?', default=None)
+    parser.add_argument('title', nargs='?', default=None)
 
+    args = parser.parse_args(wf.args)
+
+    if args.story:
+        show_story_options(args.story)
+
+    elif args.subtask:
+        show_story_options(args.subtask, is_subtask=True)
+
+    elif args.subtasks:
+        list_subtasks(args.subtasks)
+
+    elif args.add_subtask:
+        show_subtask_options(args.add_subtask, args.title)
+
+    # elif args.save_subtask:
+    #     create_subtask(wf.stored_data('key'), wf.stored_data('subtask_title'))
+
+    else:
+        list_incomplete_stories()
+
+    wf.send_feedback()
 
 if __name__ == '__main__':
     wf = Workflow()
+    wf.data_serializer = 'json'
     log = wf.logger
     board_id = wf.settings.get('sprint_board', None)
-    jira = setup(wf)
+    jira = setup()
     sys.exit(wf.run(main))
